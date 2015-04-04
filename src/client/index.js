4139ae4d5c27a61abcda8bd05ed174903e1a7472
Peter Schroedl <peter_schroedl@me.com>
2015-04-03 20:42:49 -0700

    Post to home when / is loaded ... for testing :)

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,10 @@ var slateApp = {};
 
 slateApp.onReady = function() {
 	console.log('successfully loaded bundle.js');
+
+    $.post( "http://50.250.227.5/switch", function( data ) {
+      $( "#speaker_toggle_response" ).html( data );
+    });
 };
 
 $(document).ready(slateApp.onReady);
